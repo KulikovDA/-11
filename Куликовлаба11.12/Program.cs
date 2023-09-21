38c3b4b46644b78bee352128f2ba481c6ca6d6b0
Arion <Arion@DESKTOP-9BU80RS>
2023-09-21 16:17:05 +0200

    Добавьте файлы проекта.

---
+++ Куликовлаба11.12/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите z:");
+double z = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y:");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите c:");
+double c = double.Parse(Console.ReadLine());
+double G = (Math.Tan(Math.Pow(x,3)-6-Math.Pow(Math.Cos(z+x*y),3) / (Math.Pow(Math.Cos(Math.Pow(x,3))*Math.Pow(c,2),4))));
