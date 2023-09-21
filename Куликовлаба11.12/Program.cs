8f63100afefe294954da2282d5d3fdc7dd799d98
KulikovDA <144238632+KulikovDA@users.noreply.github.com>
2023-09-21 16:19:04 +0200

    Update Program.cs

--- Куликовлаба11.12/Program.cs
+++ Куликовлаба11.12/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите x:");
+//Высокий уровень 5
+Console.WriteLine("Введите x:");
 double x = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите z:");
 double z = double.Parse(Console.ReadLine());
@@ -7,3 +8,4 @@ double y = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите c:");
 double c = double.Parse(Console.ReadLine());
 double G = (Math.Tan(Math.Pow(x,3)-6-Math.Pow(Math.Cos(z+x*y),3) / (Math.Pow(Math.Cos(Math.Pow(x,3))*Math.Pow(c,2),4))));
+Console.WriteLine($"G={G}");
